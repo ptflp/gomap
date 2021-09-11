5f64784e5e230ab9f8f3191d6ac54ea29e45a741
ptflp <globallinkliberty@gmail.com>
2021-09-12 02:34:46 +0900

    upload

--- implementation.go
+++ implementation.go
@@ -17,15 +17,23 @@ func init() {
 	decoder = godecoder.NewDecoder(config)
 }
 
-func MapStructs(dest, src interface{}) error {
+func MapStructs(dest, src interface{}, tag ...string) error {
+	d := decoder
+	if len(tag) > 0 {
+		config := jsoniter.Config{
+			EscapeHTML: true,
+			TagKey:     tag[0],
+		}
+		d = godecoder.NewDecoder(config)
+	}
 	var b bytes.Buffer
 
-	err := decoder.Encode(&b, src)
+	err := d.Encode(&b, src)
 	if err != nil {
 		return err
 	}
 
-	err = decoder.Decode(&b, dest)
+	err = d.Decode(&b, dest)
 	if err != nil {
 		return err
 	}
